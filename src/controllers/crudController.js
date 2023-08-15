ead938cc0be19aa6e40206549c4fb2f11f604c4c
Franco <abantoroman@gmail.com>
2023-08-15 15:41:17 -0500

    CRUD completo

--- src/controllers/crudController.js
+++ src/controllers/crudController.js
@@ -5,7 +5,7 @@ const verifyToken = require("./verifyToken");
 
 router.get("/listarEntidades", verifyToken, (req, res, next) => {
   const procedureName = "listar_entidades";
-  const query = `CALL ${procedureName}();`;
+  const query = `CALL ${procedureName}()`;
 
   connection.query(query, (error, results) => {
     if (error) {
@@ -59,11 +59,83 @@ router.post("/crearEntidad", verifyToken, (req, res, next) => {
   );
 });
 
+router.get("/verEntidad", verifyToken, (req, res, next) => {
+  const { id_entidad } = req.body;
+  const procedureName = "ver_entidad";
+  const query = `CALL ${procedureName}(?)`;
+
+  connection.query(query, [id_entidad], (error, results) => {
+    if (error) {
+      console.error("Error al llamar al procedimiento almacenado:", error);
+      res.status(500).json({ message: "Error en el servidor" });
+    } else {
+      if (results[0].length > 0) {
+        res.json(results[0]);
+      } else {
+        res.json({ message: "No hay datos" });
+      }
+    }
+  });
+});
+
+router.post("/actualizarEntidad", verifyToken, (req, res, next) => {
+  const {
+    id_entidad,
+    id_tipo_documento,
+    nro_documento,
+    razon_social,
+    nombre_comercial,
+    id_tipo_contribuyente,
+    direccion,
+    telefono,
+  } = req.body;
+
+  const procedureName = "actualizar_entidad";
+  const values = [
+    id_entidad,
+    id_tipo_documento,
+    nro_documento,
+    razon_social,
+    nombre_comercial,
+    id_tipo_contribuyente,
+    direccion,
+    telefono,
+  ];
+
+  connection.query(
+    `CALL ${procedureName}(?, ?, ?, ?, ?, ?, ?, ?)`,
+    values,
+    (error, results) => {
+      if (error) {
+        console.error("Error al actualizar entidad:", error);
+        res.status(500).json({ message: "Error en el servidor" });
+      } else {
+        res.status(200).json({ message: "Entidad actualizada correctamente" });
+      }
+    }
+  );
+});
+
+router.delete("/eliminarEntidad/:id_entidad", verifyToken, (req, res, next) => {
+  const id_entidad = req.params.id_entidad;
+  const procedureName = "eliminar_entidad";
+  const query = `CALL ${procedureName}(?)`;
+
+  connection.query(query, [id_entidad], (error, results) => {
+    if (error) {
+      console.error("Error al llamar al procedimiento almacenado:", error);
+      res.status(500).json({ message: "Error en el servidor" });
+    } else {
+      res.status(200).json({ message: "Entidad eliminada correctamente" });
+    }
+  });
+});
+
 //Listar para los select del formulario
 
 router.get("/listarTipoContribuyente", verifyToken, (req, res, next) => {
   const procedureName = "listar_tipo_contribuyente";
-  const query = `CALL ${procedureName}();`;
+  const query = `CALL ${procedureName}()`;
 
   connection.query(query, (error, results) => {
     if (error) {
@@ -81,7 +153,7 @@ router.get("/listarTipoContribuyente", verifyToken, (req, res, next) => {
 
 router.get("/listarTipoDocumento", verifyToken, (req, res, next) => {
   const procedureName = "listar_tipo_documento";
-  const query = `CALL ${procedureName}();`;
+  const query = `CALL ${procedureName}()`;
 
   connection.query(query, (error, results) => {
     if (error) {
